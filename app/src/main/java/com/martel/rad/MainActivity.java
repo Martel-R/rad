bb2ad02ddbdbbd5ed0e58b9c921d94ed4151fa00
Enrique Martel <enrique@meta.edu.br>
2018-08-31 16:51:32 -0300

    Iniciado o firebase com as dependencias:   - Database Realtime;   - Authentication;   - Storage.

--- app/src/main/java/com/martel/rad/MainActivity.java
+++ app/src/main/java/com/martel/rad/MainActivity.java
@@ -3,7 +3,10 @@ package com.martel.rad;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
